7849b6e523a4a6299fa28577e1b233eebd58120e
Ivan-Platonov <44867311+Ivan-Platonov@users.noreply.github.com>
2023-10-25 08:21:16 +0000

    task_4 is complite

--- task_4.py
+++ task_4.py
@@ -4,6 +4,19 @@ print('Задача 4. Площадь треугольника')
 # которая запрашивает у пользователя длины двух катетов
 # в прямоугольном треугольнике и выводит его площадь.
 
-# Формула: 
+# Формула:
 # S = ab/2
 
+first_cathet, second_cathet = ('', ) * 2
+
+print('')
+while first_cathet.strip() == '':
+  first_cathet = input('Введите длину первого катета: ')
+while second_cathet.strip() == '':
+  second_cathet = input('Введите длину второго катета: ')
+print('')
+
+square = (float(first_cathet) * float(second_cathet)) / 2
+print('Площадь прямоугольного треугольника равна', square)
+
+exit()
