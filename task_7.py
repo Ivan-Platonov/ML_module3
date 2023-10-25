1552f045b5f9a03df9a706bb30f029646cb2c641
Ivan-Platonov <44867311+Ivan-Platonov@users.noreply.github.com>
2023-10-25 10:35:23 +0000

    Input immediately in integer type and add error message if input the string

--- task_7.py
+++ task_7.py
@@ -7,27 +7,29 @@ print('Задача 7. Режем число на части')
 # Само число при этом изменять нельзя, то есть нужно обойтись без переприсваивания.
 # Однако можно использовать сколько угодно переменных
 
-full_digit = ''
+full_digit = 0
 
 print('')
-while full_digit == '' or len(full_digit) != 4:
-  full_digit = input('Введите четырехзначное число: ')
+while len(str(full_digit)) != 4:
+  try:
+    full_digit = int(input("Введите четырехзначное число: "))
+    if len(str(full_digit)) != 4:
+      print("Вы ввели не 4 знака!")
+  except ValueError:
+    print("Вы ввели некорректное значение!")
 print('')
-
-int_full_digit = int(full_digit)
-
-first_digit = int((int_full_digit - int_full_digit % 1000) / 1000)
-second_digit = int((int_full_digit % 1000 - int_full_digit % 100) / 100)
-third_digit = int((int_full_digit % 100 - int_full_digit % 10) / 10)
-fourth_digit = (int_full_digit % 10)
 
 #Первый вариант:
+first_digit = int((full_digit - full_digit % 1000) / 1000)
+second_digit = int((full_digit % 1000 - full_digit % 100) / 100)
+third_digit = int((full_digit % 100 - full_digit % 10) / 10)
+fourth_digit = (full_digit % 10)
+
 print('Число состоит из цифр:')
 print(
   str(first_digit) + ',',
   str(second_digit) + ',',
-  str(third_digit) + ',', 
-  str(fourth_digit))
+  str(third_digit) + ',', str(fourth_digit))
 
 #Второй вариант:
 #print('Число состоит из цифр:')
